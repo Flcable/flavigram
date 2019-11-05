c9f87d7188014b17f8afcdb49d00e7d6e2bde0e2
Flcable <flcable@gmail.com>
2019-11-05 00:17:42 +0000

    Integração Firebase, erros de TS corrigidos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,21 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
+export const environment = {
+  firebase: {
+    apiKey: "AIzaSyDD6i2ycuLtQzrAbit8KTERfH3TA4hfXJA",
+    authDomain: "flavigram.firebaseapp.com",
+    databaseURL: "https://flavigram.firebaseio.com",
+    projectId: "flavigram",
+    storageBucket: "flavigram.appspot.com",
+    messagingSenderId: "14065031062"
+    //appId: "1:14065031062:web:aa0dff29699109cee46fb7"
+  }
+};
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
@@ -26,7 +41,10 @@ import { ProfilePage } from '../pages/profile/profile';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
